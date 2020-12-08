31cb0b83792d80f156c1f19ac79a4187d0e96d0d
DennisKipkirui <kipkiruimutai33@gmail.com>
2020-12-08 08:48:13 +0300

    fix errors

--- awards/urls.py
+++ awards/urls.py
@@ -1,31 +1,25 @@
-"""awards URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import url,include 
-from django.contrib import admin
-from django.contrib.auth import views
-from django.conf import settings
+from . import views
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView,  LogoutView
 from django.conf.urls.static import static
+from django.conf import settings
 
 
-urlpatterns = [
-    url('admin/', admin.site.urls),
-    url(r'', include('awards.urls')),
+
+urlpatterns=[
     
- 
+    url(r'^$', views.index, name='homepage'),
+    url('accounts/profile/$', views.myProfile, name='profile'),
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^details/(\d+)$', views.details, name='details'),
+    url(r'^api/profile/$', views.Profile_list.as_view()),
+    url(r'^api/projects/$', views.Project_list.as_view()),
+    url(r'^profile/$',views.profile_edit,name='edit_profile'),
+    # url('project/review/<project_id>',views.project_review,name='project_review'),
+    url('accounts/login/',LoginView.as_view(redirect_authenticated_user=True,template_name='accounts/login.html'),name='login'),
+    url('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+    url('accounts/register/',views.register, name='register'),
+   
+
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
